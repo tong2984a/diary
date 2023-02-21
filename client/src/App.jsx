76088b6ec5346c17e434041eec1dc9e92cfa3573
tong2984a <tong2984a@gmail.com>
2023-02-21 18:13:42 +0800

    init diary

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Transactions />
+    </div>
+  </div>
+);
+
+export default App;
